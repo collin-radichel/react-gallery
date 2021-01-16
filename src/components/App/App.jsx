0158b1997478049715554f01cc9cf767c86a10b4
collinradichel <radichel.collin@gmail.com>
2021-01-16 16:16:32 -0600

    added photos for project and started working on GET route to see images on the DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import './App.css';
+import GalleryList from ''
 
 function App() {
+
+  let [gallery, setGallery] = useState([]);
+
+   //on load
+   useEffect(() => {
+    getGallery()
+}, [])
+
+
+  // GET function 
+  const getGallery = () => {
+    axios.get('/gallery').then(response => {
+      setGallery(response.data)
+      console.log(response.data);
+    }).catch(err =>{
+      console.log(err);
+    })
+  }
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -9,6 +33,7 @@ function App() {
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <Button variant="secondary">Secondary</Button>
       </div>
     );
 }
